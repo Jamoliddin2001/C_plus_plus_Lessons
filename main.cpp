6c8de7037e212fa330e108e9e35cc4d9c954edcd
jamoliddin <sh.jamoliddin2001@gmail.com>
2023-01-25 16:16:37 +0500

    update #3

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 int main()
 {
     long a = 0;
-    std::cin>>a;
-    std::cout<<a<<" Hello, World!"<<std::endl;
-    std::cout<<"RESTART1";
+    for (int i = 0; i < 256; i++)
+    {
+        std::cout<<(char) i<<"\n";
+    }
     return 0;
 }
