3c1f8adb3734a39d12c52c539bf9efe38b528a5a
jamoliddin <sh.jamoliddin2001@gmail.com>
2023-01-27 15:23:29 +0500

    update #5

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <cmath>
+
+
+bool isPrime(int x)
+{
+    for (int i = 2; i < sqrt(x); ++i) {
+        if (x % i == 0) return false;
+    }
+    return true;
+}
 
 int main()
 {
+    std::cout<<"Test\n";
     for (int i = 0; i < 256; i++)
     {
         std::cout<<(char) i<<"\n";
