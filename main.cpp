3bced35636e0750ab06231a89fb726a680dc7d06
jamoliddin <sh.jamoliddin2001@gmail.com>
2023-02-03 09:30:50 +0500

    update #12

--- main.cpp
+++ main.cpp
@@ -8,21 +8,6 @@ int gcd(int a, int b)
     gcd(b, a % b);
 }
 
-
-
-int gcd(int a, int b)
-{
-    int temp;
-    while (b > 0)
-    {
-        temp = a % b;
-        a = b;
-        b = temp;
-    }
-
-    return a;
-}
-
 bool isPrime(int x)
 {
     for (int i = 2; i <= sqrt(x); ++i)
